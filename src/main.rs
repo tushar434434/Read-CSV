32630f5d62d51d92c73ead66bbb4b2d5d2b3f32e
Tushar Kumar <tusharkumar1036@gmail.com>
2026-06-13 16:49:51 +0530

    Explaination

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use std::error::Error;
-use csv;
+use std::error::Error;//error ko handle ke ley
+use csv; //csv ka use kr rhe hai 
 
-fn read_from_file(path : &str) -> Result<(), Box<dyn Error>> {
-    let mut reader =csv::Reader::from_path(path)?;
-    for result in reader.records(){
-        let record = result?;
-        println!("{:?}",record);
+fn read_from_file(path : &str) -> Result<(), Box<dyn Error>> {// it return error if file not found
+    let mut reader =csv::Reader::from_path(path)?;// it return error if file not found
+    for result in reader.records(){//loop chala diya hai records ke liye
+        let record = result?;// it return error if file not found
+        println!("{:?}",record);// it print the record in the file
     }
     Ok(())
 }
 fn main(){
-    if let Err(e) =read_from_file("./customers.csv"){
-        eprintln!("{}",e);
+    if let Err(e) =read_from_file("./customers.csv"){ // it give error if file not found 
+        eprintln!("{}",e); //explain the error with discription
     }
 }
